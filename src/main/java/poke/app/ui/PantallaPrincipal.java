daebce02449b848fe25541be31d7a346dd2fe024
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-05-16 13:45:23 +0200

    update2

---
+++ src/main/java/poke/app/ui/PantallaPrincipal.java
@@ -0,0 +1,12 @@
+package poke.app.ui;
+
+import javax.swing.*;
+
+public class PantallaPrincipal {
+    private JPanel PantallaPrincipal;
+    private JPanel personaje;
+    private JPanel hombre;
+    private JPanel mujer;
+    private JPanel descripcionh;
+    private JPanel fotoh;
+}
