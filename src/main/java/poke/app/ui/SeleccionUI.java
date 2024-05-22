29fb09060bbccced71c547c69fee6ac8d2a8cf8d
bryanStv <bryans.oyola@gmail.com>
2024-05-22 10:14:36 +0200

    0.04 --> Cambio entre UIs y Selección

--- src/main/java/poke/app/ui/SeleccionUI.java
+++ src/main/java/poke/app/ui/SeleccionUI.java
@@ -3,22 +3,52 @@ package poke.app.ui;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.*;
 
 @Component
 public class SeleccionUI extends JFrame{
 
-    private JLabel Chico;
-    private JLabel Chica;
     private JTabbedPane SeleccionTab;
     private JLabel textoPrincipal;
     private JPanel barra1;
     private JPanel barra2;
     private JPanel bg;
     private JPanel pMain;
+    private JButton chico;
+    private JButton chica;
+    private JButton continuar1;
     private Image foto;
+
+    public SeleccionUI() {
+        chico.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        chica.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+        continuar1.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                SeleccionTab.setSelectedIndex(1);
+            }
+        });
+
+        SeleccionTab.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+            }
+        });
+    }
 
     public void main(JFrame frame) {
         frame.setContentPane(new SeleccionUI().pMain);
@@ -27,26 +57,4 @@ public class SeleccionUI extends JFrame{
         frame.pack();
         frame.setVisible(true);
     }
-
-    /*public SeleccionUI() {
-        setTitle("Seleccion");
-        JPanel panel = new JPanel();
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        setLocationRelativeTo(null);
-        setResizable(false);
-        panel.setLayout(null);
-        bg = new JPanel();
-        bg.setLayout(null);
-        barra1 = new JPanel();
-        barra1.setLayout(null);
-        barra1.setBackground(Color.WHITE);
-        barra1.setBounds(10, 10, 450, 200);
-        barra2 = new JPanel();
-        barra2.setLayout(null);
-        barra2.setBackground(Color.WHITE);
-        barra2.setBounds(10, 50, 450, 200);
-        barra1.add(barra2);
-        //setVisible(true);
-    }*/
 }
