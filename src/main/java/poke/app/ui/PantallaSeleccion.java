ae4597cd08720d2cef0f08d8c45048f74050c279
Leonard-Stan <adrianinformatica1920@gmail.com>
2024-05-22 10:37:12 +0200

    TercerPushTestAdrian
    Cambios Menores

--- src/main/java/poke/app/ui/PantallaSeleccion.java
+++ src/main/java/poke/app/ui/PantallaSeleccion.java
@@ -1,8 +1,10 @@
 package poke.app.ui;
 import java.awt.*;
 import javax.swing.*;
-public class PantallaSeleccion {
+import javax.swing.border.Border;
 
+public class PantallaSeleccion {
+    private JButton buscarButton;
 
     public PantallaSeleccion(JFrame frame)
 {
@@ -10,10 +12,19 @@ public class PantallaSeleccion {
     frame.setContentPane(panel);
     panel.setLayout(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(1920, 1080);
+    frame.setSize(1080, 720);
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
     frame.setTitle("seleccion personaje");
+
+    Border layoutPrincipal = BorderFactory.createLineBorder(Color.black);
+
+    buscarButton = new JButton();
+    buscarButton.setIcon(new ImageIcon(getClass().getResource("")));
+    buscarButton.setBorderPainted(false);
+    buscarButton.setFocusPainted(false);
+    buscarButton.setContentAreaFilled(false);
+
 }
 
 
