807b208c6885fa73efabe10e129d822146b26cb7
bryanStv <bryans.oyola@gmail.com>
2024-06-06 13:38:32 +0200

    Cambios Eduard,Adrián y Bryan
    SetResisable(false)

--- src/main/java/poke/app/localData/Window.java
+++ src/main/java/poke/app/localData/Window.java
@@ -6,6 +6,7 @@ public class Window extends JFrame {
     public static JFrame frame = new JFrame();
     public Window() {
         frame.setSize(800, 900);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
