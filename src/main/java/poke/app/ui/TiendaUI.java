4159dd63b97a7619dc92d62f67749383d401d350
Leonard-Stan <adrianinformatica1920@gmail.com>
2024-06-11 09:23:17 +0200

    Arrays pokemons: Jefes, enemigos y para comprar

---
+++ src/main/java/poke/app/ui/TiendaUI.java
@@ -0,0 +1,12 @@
+package poke.app.ui;
+
+import javax.swing.*;
+
+public class TiendaUI {
+    private JPanel panelPrincipal;
+    private JTabbedPane Tienda;
+    private JPanel mejoras;
+    private JPanel pokemon;
+
+
+}
